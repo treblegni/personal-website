f06bbc9e5e54c794fe45c19cea07d18ce019a98b
treblegni <ingelbert.figueroa@gmail.com>
2020-05-29 12:58:08 -0700

    Updates

---
+++ precache-manifest.8c32a8f4f3dcf29be2422f7db0a9a8e6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506441e590b6c06e71f8d2a2c37f17a8",
+    "url": "/ingelbert.codes/index.html"
+  },
+  {
+    "revision": "16e5692696d344bac83b",
+    "url": "/ingelbert.codes/static/css/main.274217db.chunk.css"
+  },
+  {
+    "revision": "bfaa9313e9d60c0bb324",
+    "url": "/ingelbert.codes/static/js/2.8c66727f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ingelbert.codes/static/js/2.8c66727f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e5692696d344bac83b",
+    "url": "/ingelbert.codes/static/js/main.0554ac2a.chunk.js"
+  },
+  {
+    "revision": "b407472fa00c2f6bc992",
+    "url": "/ingelbert.codes/static/js/runtime-main.7d562d8b.js"
+  },
+  {
+    "revision": "8e2dcb3caf6d677fe8dd51d6e1ab0412",
+    "url": "/ingelbert.codes/static/media/cogs.8e2dcb3c.svg"
+  },
+  {
+    "revision": "abe4faa2d165476640eab6b44ffa6e90",
+    "url": "/ingelbert.codes/static/media/github.abe4faa2.svg"
+  },
+  {
+    "revision": "6521073fe1007a31580ff1b8e434354f",
+    "url": "/ingelbert.codes/static/media/linkedin.6521073f.svg"
+  },
+  {
+    "revision": "c11235d7ae03c64776f3584720161055",
+    "url": "/ingelbert.codes/static/media/logo-medium-white.c11235d7.svg"
+  },
+  {
+    "revision": "9a644f8bdb170eebeee62978c1369fdc",
+    "url": "/ingelbert.codes/static/media/mon.9a644f8b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2694137c569ce490fd9f256056642894.js"
+  "/ingelbert.codes/precache-manifest.8c32a8f4f3dcf29be2422f7db0a9a8e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ingelbert.codes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
